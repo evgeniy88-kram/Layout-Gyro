1f05b498d96c73f6c1a6a06925d64225ec35451a
evgeniy88-kram <gedata88@gmail.com>
2021-02-12 11:55:37 +0200

    new

--- scripts/main.js
+++ scripts/main.js
@@ -9,33 +9,6 @@
             $('nav').toggleClass('nav__list');
             $('#hamburger').prop('checked', false);
         });
-        //
-        //Slick-slider
-        $('.reviews__slick').slick({
-            autoplay:true,
-            autoplaySpeed: 5000,
-            arrows:true,
-            dots: false,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            responsive: [{
-                breakpoint: 1250,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    arrows: false
-                }
-                },
-                {
-                    breakpoint: 900,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        arrows: false
-                    }
-            }]
-        });
-        //
         //smooth scrolling
         $("body").on('click', '[href*="#"]', function(e){
             let fixed_offset = 100;
